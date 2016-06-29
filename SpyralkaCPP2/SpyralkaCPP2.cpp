6c692f1837ce9a8e7dcb203cad0eb22b1befe626
Whice <and-not@mail.ru>
2016-06-29 16:39:01 +0300

    1st version

---
+++ SpyralkaCPP2/SpyralkaCPP2.cpp
@@ -0,0 +1,8 @@
+#include "SpyralkaCPP2.h"
+
+void main()
+{
+	int a=1;
+	printq(a);
+	cin>>a;
+}
---
+++ SpyralkaCPP2/SpyralkaCPP2.h
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+
+void printq(int a)
+{
+	cout<<a+a;
+}
